5053cd0a9c16acf5b7f0a3c4603e25bebbf137b5
Andrei Jaume <andrei.jaumewillenska@utexas.edu>
2024-10-19 17:15:03 -0500

    Add constant strings for output messages

--- include/Constants.h
+++ include/Constants.h
@@ -13,8 +13,10 @@
 #include <unordered_map>
 
 namespace Constants{
-    constexpr int CLI_ARGUMENTS = 2;
+    // CLI Arguments
+    constexpr int CLI_ARGUMENTS = 1;
 
+    // Commands
     enum class Command {
         START,
         UNLOAD,
@@ -27,4 +29,13 @@ namespace Constants{
         {"unload", Command::UNLOAD},
         {"exit", Command::EXIT}
     };
+
+    // Output Messages
+    const std::string USAGE_MESSAGE = "Usage: FileEventsReporter [start unload exit]";
+    const std::string INVALID_INPUT_COMMAND = "Invalid input command";
+    const std::string EXECUTION_COMPLETE = "FileEventsReporter execution complete";
+    const std::string EXIT_MESSAGE = "Exiting FileEventsReporter...";
+    const std::string START_MESSAGE = "Starting FileEventsReporter...";
+    const std::string UNLOAD_MESSAGE = "Unloading FileEventsReporter...";
+    const std::string INVALID_COMMAND_MESSAGE = "Invalid command. Please enter a valid command.";
 };
