500f4bfe1abd594ec4f4cb2cf5da76a06a2981b6
Andrei Jaume <andrei.jaumewillenska@utexas.edu>
2024-10-20 11:56:47 -0500

    Update the CLI logic

--- src/FileEventsReporter.cpp
+++ src/FileEventsReporter.cpp
@@ -10,8 +10,11 @@
 * The program uses the FileEventsMinifilter class to scan the RPC file events on the system.
 */
 
-#include <string>
+#include <chrono>
 #include <iostream>
+#include <limits>
+#include <string>
+#include <thread>
 
 #include "Constants.h"
 #include "FilterLoader.h"
@@ -19,6 +22,10 @@
 
 using namespace std;
 
+/// @brief Validate the User input arguments
+/// @param argc 
+/// @param argv 
+/// @return True if the arguments are valid, false otherwise
 static bool validateArguments(int argc, char* argv[]){
     // Ensure the user has provided the correct number of arguments.
 	if (argc != Constants::CLI_ARGUMENTS)
@@ -29,6 +36,16 @@ static bool validateArguments(int argc, char* argv[]){
 	return true;
 }
 
+/// @brief Check to see if user input is available
+/// @return True if user input is available, false otherwise
+bool userInputAvailable() {
+    return !std::cin.eof();
+}
+
+/// @brief CLI Main entry point
+/// @param argc 
+/// @param argv 
+/// @return Exit code.
 int main(int argc, char* argv[])
 {
 	// Validate the input arguments
@@ -49,31 +66,34 @@ int main(int argc, char* argv[])
     Constants::Command command;
     while (cliRunning) {
         // Validate the input command
-        getline(cin, commandString);        
-        if (!StringValidationHelper::isCommandValid(commandString, command))
-        {
-            cerr << Constants::INVALID_INPUT_COMMAND << endl;
-            continue;
-        }
+        if (userInputAvailable()) { // Non-cross-platform call. May need to update to curses.h or have platform specific calls for this.
+            getline(cin, commandString);     
+            if (!StringValidationHelper::isCommandValid(commandString, command))
+            {
+                cerr << Constants::INVALID_INPUT_COMMAND << endl;
+                continue;
+            }
 
-        switch (command) {
-            case Constants::Command::START:
-                std::cout << Constants::START_MESSAGE << std::endl;
-                filterLoader.StartFilter();
-                break;
-            case Constants::Command::UNLOAD:
-                std::cout << Constants::UNLOAD_MESSAGE << std::endl;
-                filterLoader.StopFiler();
-                break;
-            case Constants::Command::EXIT:
-                std::cout << Constants::EXIT_MESSAGE << std::endl;
-                filterLoader.StopFiler();
-                cliRunning = false;
-                break;
-            default:
-                cerr << Constants::INVALID_COMMAND_MESSAGE << argv[1] << endl;
-                return 1;
-	    }
+            switch (command) {
+                case Constants::Command::START:
+                    std::cout << Constants::START_MESSAGE << std::endl;
+                    filterLoader.StartFilter();
+                    break;
+                case Constants::Command::UNLOAD:
+                    std::cout << Constants::UNLOAD_MESSAGE << std::endl;
+                    filterLoader.StopFiler();
+                    break;
+                case Constants::Command::EXIT:
+                    std::cout << Constants::EXIT_MESSAGE << std::endl;
+                    filterLoader.StopFiler();
+                    cliRunning = false;
+                    break;
+                default:
+                    cerr << Constants::INVALID_COMMAND_MESSAGE << argv[1] << endl;
+                    return 1;
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Sleep for 100ms to prevent busy waiting
     }
 
     // Wait for all threads to finish
