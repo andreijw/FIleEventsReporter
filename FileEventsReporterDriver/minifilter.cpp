8947fe85d51a28c9cf7e3ba4e44cb0dace38df7a
Andrei Jaume <andrei.jaumewillenska@utexas.edu>
2024-10-20 14:26:30 -0500

    setup the driver structure

--- FileEventsReporterDriver/minifilter.cpp
+++ FileEventsReporterDriver/minifilter.cpp
@@ -1,6 +1,5 @@
 #include <fltKernel.h>
 
-
 // Global variables
 PFLT_FILTER gFilterHandle;
 ULONG_PTR OperationStatusCtx = 1;
@@ -20,7 +19,7 @@ InstanceSetupCallback(
     _In_ FLT_FILESYSTEM_TYPE VolumeFilesystemType
 );
 
-VOID
+NTSTATUS
 InstanceTeardownCallback(
     _In_ PCFLT_RELATED_OBJECTS FltObjects,
     _In_ FLT_INSTANCE_TEARDOWN_FLAGS Reason
@@ -45,3 +44,46 @@ NTSTATUS
 UnloadDriver(
     _In_ FLT_FILTER_UNLOAD_FLAGS Flags
 );
+
+// Registration structure
+// IRP events https://learn.microsoft.com/en-us/previous-versions/windows/drivers/ifs/completing-the-irp
+const FLT_OPERATION_REGISTRATION Callbacks[] = {
+    { IRP_MJ_CREATE,
+      0,
+      PreOperationCallback,
+      PostOperationCallback },
+
+    { IRP_MJ_WRITE,
+      0,
+      PreOperationCallback,
+      PostOperationCallback },
+
+    { IRP_MJ_READ,
+      0,
+      PreOperationCallback,
+      PostOperationCallback },
+
+    { IRP_MJ_SET_INFORMATION,
+      0,
+      PreOperationCallback,
+      PostOperationCallback },
+
+    { IRP_MJ_OPERATION_END }
+};
+
+const FLT_REGISTRATION FilterRegistration = {
+    sizeof(FLT_REGISTRATION),         // Size
+    FLT_REGISTRATION_VERSION,         // Version
+    0,                                // Flags
+    NULL,                             // Context
+    Callbacks,                        // Operation callbacks
+    UnloadDriver,                     // MiniFilterUnload
+    InstanceSetupCallback,            // InstanceSetup
+    InstanceTeardownCallback,         // InstanceTeardownStart
+    NULL,                             // InstanceTeardownComplete
+    NULL,                             // GenerateFileName
+    NULL,                             // GenerateDestinationFileName
+    NULL                              // NormalizeNameComponent
+};
+
+// Implementation
